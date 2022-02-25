c5cc8a93a3ddbb3f14cb7ee41762b753a635b63a
kyeongmin <8721son@naver.com>
2022-02-25 13:37:12 +0900

    init

---
+++ src/main/java/com/example/wildfly2/Wildfly2Application.java
@@ -0,0 +1,13 @@
+package com.example.wildfly2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wildfly2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wildfly2Application.class, args);
+    }
+
+}
